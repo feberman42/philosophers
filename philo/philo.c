a2924d2a8560605d047a5fe7831a480b058000cd
feberman42 <feberman@student.42berlin.de>
2024-01-04 17:28:57 +0100

    main file

---
+++ philo/philo.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 10:53:58 by feberman          #+#    #+#             */
+/*   Updated: 2024/01/04 17:11:35 by feberman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*routine(void *p)
+{
+	(void)p;
+	printf("Hello\n");
+	return (NULL);
+}
+
+int	main(void)
+{
+	pthread_t	t1;
+
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	usleep(0);
+	return (0);
+}
