18ecc1ee85968b2ede11e91711ec266b071abe2a
feberman42 <feberman@student.42berlin.de>
2024-01-05 12:07:18 +0100

    more protos

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 16:43:43 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/05 09:52:41 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 12:00:53 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ t_data	*malloc_data(void);
 t_data	*validate_input(int argc, char *argv[]);
 void	*fill_data(int argc, char *argv[], t_data *data);
 int	convert_input_num(char *str, unsigned int *target);
+void	convert_timescale(t_data *data);
 
 //utils.c
 unsigned int	ft_strlen(const char *str);
@@ -45,4 +46,8 @@ unsigned int	ft_strlen(const char *str);
 //errors.c
 void	*ft_error(const char *msg);
 void	*eval_input_err(int	code);
+
+//debug.c
+void	print_data(t_data *data);
+
 #endif
