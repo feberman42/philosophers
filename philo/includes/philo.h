6b3c4b1f99b53e1615c933a3bede67a5f899b777
feberman42 <feberman@student.42berlin.de>
2024-01-05 12:33:05 +0100

    added time

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 16:43:43 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/05 12:00:53 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 12:30:48 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <limits.h>
+# include <sys/time.h>
 # include "errors.h"
 
 typedef	struct	s_data
@@ -29,6 +30,7 @@ typedef	struct	s_data
 	unsigned int	time_to_sleep;
 	unsigned int	eat_count;
 	int				use_eat_count;
+	long long int		time_offset;
 }	t_data;
 
 //structs.c
@@ -37,11 +39,15 @@ t_data	*malloc_data(void);
 //input.c
 t_data	*validate_input(int argc, char *argv[]);
 void	*fill_data(int argc, char *argv[], t_data *data);
-int	convert_input_num(char *str, unsigned int *target);
+int		convert_input_num(char *str, unsigned int *target);
 void	convert_timescale(t_data *data);
 
 //utils.c
 unsigned int	ft_strlen(const char *str);
+
+//time.c
+int			set_time_offset(t_data *data);
+long int	get_time(long int offset);
 
 //errors.c
 void	*ft_error(const char *msg);
