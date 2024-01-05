bedeba60bbc6e447eea9d2426d8a4a0d3a03367b
feberman42 <feberman@student.42berlin.de>
2024-01-05 10:10:18 +0100

    added first err_funcs

--- philo/src/errors.c
+++ philo/src/errors.c
@@ -6,7 +6,25 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:26:54 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/04 11:26:55 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 09:45:17 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+void	*ft_error(const char *msg)
+{
+	int	i;
+
+	i = 0;
+	while (msg[i])
+		write(2, msg + i++, 1);
+	return (NULL);
+}
+
+void	*eval_input_err(int	code)
+{
+	if (code == 1)
+		return (ft_error(ERR_NOT_NUM));
+	return (ft_error(ERR_NUM_BIG));
+}
