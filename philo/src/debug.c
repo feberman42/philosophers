12973e1fc23ae9fee79c1ff56d78d8c28a03eedc
feberman42 <feberman@student.42berlin.de>
2024-01-05 12:33:13 +0100

    added time to debug

--- philo/src/debug.c
+++ philo/src/debug.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 11:54:01 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/05 12:06:36 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 12:31:38 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	print_data(t_data *data)
 {
-	printf("Philosophers: %u\nTime_to_die: %u\nTime_to eat: %u\nTime_to_sleep: %u\nTimes_to_eat: %u\nUse eats: %i\n",\
-			data->philo_count, data->time_to_die, data->time_to_eat, data->time_to_sleep, data->eat_count, data->use_eat_count);
+	printf("Philosophers: %u\nTime_to_die: %u\nTime_to eat: %u\nTime_to_sleep: %u\nTimes_to_eat: %u\nUse eats: %i\nTime: %lli\n",\
+			data->philo_count, data->time_to_die, data->time_to_eat, data->time_to_sleep, data->eat_count, data->use_eat_count, data->time_offset);
 }
