88ca72a840ab8d3de2b23c8464c81691a603461e
feberman42 <feberman@student.42berlin.de>
2024-01-05 16:09:38 +0100

    creation of arr

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 17:13:54 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/04 17:15:09 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 15:55:40 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,42 @@ unsigned int	ft_strlen(const char *str)
 	while (str[len])
 		len++;
 	return (len);
+}
+
+t_philo	*get_philos_arr(t_data *data)
+{
+	t_philo			*philos;
+	unsigned int	i;
+
+	philos = malloc(sizeof(t_philo) * data->philo_count);
+	if (!philos)
+	{
+		free(data);
+		return (ft_error(ERR_MALLOC));
+	}
+	i = 0;
+	while (i < data->philo_count)
+	{
+		philos[i].id = i + 1;
+		philos[i].data = data;
+		i++;
+	}
+	return (philos);
+}
+
+int	*get_states_arr(t_data *data)
+{
+	int	*states;
+	unsigned int	i;
+
+	states = malloc(sizeof(int) * data->philo_count);
+	if (!states)
+	{
+		free(data);
+		return (ft_error(ERR_MALLOC));
+	}
+	i = 0;
+	while (i < data->philo_count)
+		states[i++] = ALIVE;
+	return (states);
 }
