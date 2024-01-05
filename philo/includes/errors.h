f0c89995bf379ecc8a23d395dddecebdfe7f64a8
feberman42 <feberman@student.42berlin.de>
2024-01-05 12:32:58 +0100

    new error time

--- philo/includes/errors.h
+++ philo/includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 09:14:20 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/05 09:34:41 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 12:21:34 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # define ERR_NUM_BIG	"Input parameter too big."
 # define ERR_MALLOC		"Memory allocation failed."
 # define ERR_ARG_NUM	"Wrong number of arguments."
+# define ERR_TIME		"Couldn't get time."
 
 #endif
