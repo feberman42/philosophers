e4f310e33450469836663035f77f74811610fa89
feberman42 <feberman@student.42berlin.de>
2024-01-05 10:11:42 +0100

    header for err_msgs

---
+++ philo/includes/errors.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 09:14:20 by feberman          #+#    #+#             */
+/*   Updated: 2024/01/05 09:34:41 by feberman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERRORS_H
+# define ERRORS_H
+
+# define ERR_NOT_NUM	"Input is not a positive integer."
+# define ERR_NUM_BIG	"Input parameter too big."
+# define ERR_MALLOC		"Memory allocation failed."
+# define ERR_ARG_NUM	"Wrong number of arguments."
+
+#endif
