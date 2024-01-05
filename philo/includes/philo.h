9779e0bde23ea3d37d100e980b80b4b3b6ce08a3
feberman42 <feberman@student.42berlin.de>
2024-01-05 16:08:54 +0100

    protos and philo struct

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 16:43:43 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/05 12:30:48 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/05 16:00:21 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@
 # include <sys/time.h>
 # include "errors.h"
 
+# define DEAD 0
+# define ALIVE 1
+
 typedef	struct	s_data
 {
 	unsigned int	philo_count;
@@ -30,8 +33,18 @@ typedef	struct	s_data
 	unsigned int	time_to_sleep;
 	unsigned int	eat_count;
 	int				use_eat_count;
-	long long int		time_offset;
+	long long int	time_offset;
+	int				*states;
+	struct s_philo	*philos;
 }	t_data;
+
+typedef struct	s_philo
+{
+	unsigned int	id;
+	t_data			*data;
+}	t_philo;
+
+void	*routine(void *p);
 
 //structs.c
 t_data	*malloc_data(void);
@@ -42,8 +55,13 @@ void	*fill_data(int argc, char *argv[], t_data *data);
 int		convert_input_num(char *str, unsigned int *target);
 void	convert_timescale(t_data *data);
 
+//threads.c
+int	launch_threads(t_data *data);
+
 //utils.c
 unsigned int	ft_strlen(const char *str);
+t_philo	*get_philos_arr(t_data *data);
+int	*get_states_arr(t_data *data);
 
 //time.c
 int			set_time_offset(t_data *data);
@@ -55,5 +73,6 @@ void	*eval_input_err(int	code);
 
 //debug.c
 void	print_data(t_data *data);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
