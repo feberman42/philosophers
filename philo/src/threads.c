04e9a433b963acdeaed677a72b79c562a71ced97
feberman42 <feberman@student.42berlin.de>
2024-01-05 16:09:29 +0100

    file for thread related stuff

---
+++ philo/src/threads.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 15:35:19 by feberman          #+#    #+#             */
+/*   Updated: 2024/01/05 15:58:10 by feberman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	launch_threads(t_data *data)
+{
+	pthread_t	t;
+	unsigned int	i;
+
+	i = 0;
+	while (i < data->philo_count)
+	{
+		pthread_create(&t, NULL, &routine, data->philos + i);
+		pthread_detach(t);
+		i++;
+	}
+	return (0);
+}
