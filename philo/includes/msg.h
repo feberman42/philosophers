2bf5b82bcdef25162409ed4745eb500deb53358b
Freddy Ideapad <theebilein@gmail.com>
2024-01-10 22:55:58 +0100

    msg complete

--- philo/includes/msg.h
+++ philo/includes/msg.h
@@ -6,14 +6,18 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:48:51 by feberman          #+#    #+#             */
-/*   Updated: 2024/01/09 19:58:45 by feberman         ###   ########.fr       */
+/*   Updated: 2024/01/10 17:24:34 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSG_H
 # define MSG_H
 
-# define THINK "is thinking."
-# define DEATH "is dead."
+# define THINK "is thinking"
+# define DEATH "died"
+# define FORK "has taken a fork"
+# define EAT "is eating"
+# define SLEEP "is sleeping"
+# define FORK_FAIL "couldn't grab forks."
 
 #endif
